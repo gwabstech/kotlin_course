2b4ab7841806ad69552f669228a16af1d0da38da
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-08-17 02:34:54 +0100

    done with function.kt

--- src/main/kotlin/Lambda functions.kt
+++ src/main/kotlin/Lambda functions.kt
@@ -1,5 +1,15 @@
 fun main() {
 
+
+
+    //println(myLambda("HABU"))
+
+    sayHello("Abubakar") {name: String -> println("hello $name") }
+}
+
+fun sayHello(name: String,doMe:(String) -> Unit){
+    doMe(name)
+
 }
 
 // Lambda
