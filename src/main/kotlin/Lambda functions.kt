7b3cad6fac4706952528556e6b6314242c2d77a8
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-08-19 17:31:29 +0100

    Higher Order function.kt

--- src/main/kotlin/Lambda functions.kt
+++ src/main/kotlin/Lambda functions.kt
@@ -1,17 +1,44 @@
 fun main() {
 
 
+    val numbers = arrayListOf<Int>(10, 20, 50, 40, 60, 80)
+    numbers.forEach {
+        println("Hello $it")
+    }
+    println("old list: $numbers")
 
-    //println(myLambda("HABU"))
+    val newNumbers = update(numbers) { numbers -> numbers / 10 }
 
-    sayHello("Abubakar") {name: String -> println("hello $name") }
+    println("new numbers list: $newNumbers")
+    val l = arrayListOf<String>("Abu", "Ali", "Habu")
+
+    sayHello("Abubakar") { name: String -> println("hello $name") }
+    val msg = messages(l) { m: String -> "Hello $m how you" }
+    println(msg)
 }
 
-fun sayHello(name: String,doMe:(String) -> Unit){
+fun sayHello(name: String, doMe: (String) -> Unit) {
     doMe(name)
 
+}
+
+fun update(numbers: ArrayList<Int>, lbd: (Int) -> Int): ArrayList<Int> {
+    for (i in 0..numbers.size - 1) {
+        if (numbers[i] % 2 == 0)
+            numbers[i] = lbd(numbers[i])
+    }
+    return numbers
 }
 
 // Lambda
 
 // Higher order functions
+
+fun messages(clients: ArrayList<String>, l: (String) -> String): ArrayList<String> {
+    val messge = arrayListOf<String>()
+    for (client in clients) {
+        messge.add(l(client))
+    }
+    return messge
+}
+
